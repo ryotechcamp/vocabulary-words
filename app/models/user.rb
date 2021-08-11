43bfbf2c8e91434224bbec67e9d747e2ae960e28
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-11 14:39:21 +0900

    ユーザー管理機能ビューファイル生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
+  has_one_attached :photo
+
   with_options presence: true do
   validates :nickname
-  validates :birthday 
+  validates :birthday
+  validates :photo
   end
 
   with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
@@ -19,4 +22,5 @@ class User < ApplicationRecord
       validates :kana_first_name
       validates :kana_last_name
   end
+
 end
