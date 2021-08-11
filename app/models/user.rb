59ecbcfa216d3f087e3c0ee72d5e8c122e322fe9
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-11 18:34:00 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,8 +15,8 @@ class User < ApplicationRecord
   end
 
   with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
-      validates :kanji_first_name
-      validates :kanji_last_name
+      validates :first_name
+      validates :last_name
   end
   with_options presence: true, format: {with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width characters' } do
       validates :kana_first_name
