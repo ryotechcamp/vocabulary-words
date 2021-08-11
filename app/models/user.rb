e4c42f107e7f2f51334f6b2ed9b6659368a50536
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-11 14:22:39 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+  with_options presence: true do
+  validates :nickname
+  validates :birthday 
+  end
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+      validates :kanji_first_name
+      validates :kanji_last_name
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width characters' } do
+      validates :kana_first_name
+      validates :kana_last_name
+  end
 end
