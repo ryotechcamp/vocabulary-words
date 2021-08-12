6b6162e950d675003d63d5446a448365ebd16bcd
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-12 20:58:08 +0900

    keywordモデル単体テストコード

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,4 +1,5 @@
 class KeywordsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @users = User.all
   end
--- spec/factories/keywords.rb
+++ spec/factories/keywords.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :keyword do
-    
+    word        {Faker::Name.initials}
+    genre_id    {Faker::Number.between(from: 2, to: 11)}
+    instruction {Faker::Lorem.sentences}
+    association :user
   end
 end
