69b3ed60aeb7503d73bc99bfd96ac1a945f1d5e4
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-15 10:14:09 +0900

    Revert "ルーティングアクション設定"
    This reverts commit bddc028a0f9971248b3220c35766a78ae36433b1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "keywords#index"
-  resources :keywords, only: [:new, :create, :show] 
+  resources :keywords, only: [:new, :create] 
 end
