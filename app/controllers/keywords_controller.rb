6d9162129574c1aef774e3769f43d64d3ca85c03
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-15 10:11:26 +0900

    マイページ機能実装

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -2,4 +2,22 @@ class KeywordsController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def new
+    @keyword = Keyword.new
+  end
+
+  def create
+    @keyword = Keyword.new(keyword_params)
+      if @keyword.save
+        redirect_to user_path(current_user.id)
+      else
+        render :new
+       end
+  end
+
+  private
+  def keyword_params
+    params.require(:keyword).permit(:word, :genre_id, :instruction).merge(user_id: current_user.id)
+  end
 end
