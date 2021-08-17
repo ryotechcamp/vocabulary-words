2317d7d9b34d64cc23464dcdb12a46a10fd53314
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-17 16:56:49 +0900

    モデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_063644) do
+ActiveRecord::Schema.define(version: 2021_08_17_075411) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2021_08_12_063644) do
     t.index ["user_id"], name: "index_keywords_on_user_id"
   end
 
+  create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "keyword_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["keyword_id"], name: "index_tests_on_keyword_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,4 +72,6 @@ ActiveRecord::Schema.define(version: 2021_08_12_063644) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "keywords", "users"
+  add_foreign_key "tests", "keywords"
+  add_foreign_key "tests", "users"
 end
