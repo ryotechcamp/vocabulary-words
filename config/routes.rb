80723b33cb581e7c2c5e635ba614734ac384a38f
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-17 20:05:35 +0900

    ルーティング記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "keywords#index"
-  resources :keywords, only: [:new, :create] 
+  resources :keywords, only: [:new, :create] do
+    resources :tests, only: [:create, :destroy]
+  end
   resources :users, only: :show
 end
