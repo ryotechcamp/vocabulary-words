cd31da81c38db1151a0811a929a09dd71bc5748c
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-19 14:44:01 +0900

    外部キー設定

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,7 +2,7 @@ class Keyword < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   belongs_to :user
-  has_many   :tests
+  has_many   :tests, dependent: :destroy
 
   with_options presence: true do
     validates :word
