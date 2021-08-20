2a0a4287f0afbb9a64da0d009419f6f1b3baa230
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-20 17:54:01 +0900

    roomモデル及びテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_065458) do
+ActiveRecord::Schema.define(version: 2021_08_20_084213) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(version: 2021_08_19_065458) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "audio"
     t.index ["user_id"], name: "index_keywords_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
