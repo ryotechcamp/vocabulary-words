aa7318c5e4acb603ca62e016bf55036f9b033dbc
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-23 16:55:26 +0900

    パス変更

--- app/javascript/message.js
+++ app/javascript/message.js
@@ -1,7 +1,9 @@
 function message (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", () => {
-    
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
   });
 };
 
