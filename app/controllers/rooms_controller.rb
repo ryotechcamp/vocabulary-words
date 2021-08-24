de7fa662b50c0f9e585987d158c4f54f7f9817ce
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-24 15:33:04 +0900

    コントローラー作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index
 
   def new
     @room = Room.new
@@ -16,13 +18,18 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @message = Message.new
-    @messages = Message.order(id: "DESC")
+    user = User.find(current_user.id)
+    @messages = user.messages.includes(:user)
   end
 
   private
 
   def room_params
     params.require(:room).permit(:name).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless @user_id == current_user.id
   end
 
 end
--- app/javascript/message.js
+++ app/javascript/message.js
@@ -1,9 +1,16 @@
 function message (){
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", () => {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/rooms/:room_id/messages", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      console.log(XHR.response);
+    };
   });
 };
 
