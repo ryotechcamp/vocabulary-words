9ca7f3e0a7d0f317afb38b43a680f0ff69f5516b
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-26 11:56:41 +0900

    testモデル単体テストコード

--- spec/factories/tests.rb
+++ spec/factories/tests.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :test do
-    
+    association :user
+    association :keyword
   end
 end
--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Test, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @test = FactoryBot.build(:test)
+  end
+
+  describe 'テスト結果' do
+      it 'userが紐付いていないと保存できない' do
+        @test.user = nil
+        @test.valid?
+        expect(@test.errors.full_messages).to include('User must exist')
+      end
+      it 'keywordが紐付いていないと保存できない' do
+        @test.keyword = nil
+        @test.valid?
+        expect(@test.errors.full_messages).to include('Keyword must exist')
+      end
+  end
 end
