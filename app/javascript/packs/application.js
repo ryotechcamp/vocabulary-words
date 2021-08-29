cbfc23ed43fa29f375453f7c0e778703a44c259a
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-08-29 11:12:15 +0900

    jQuery導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("@fortawesome/fontawesome-free");
 import '@fortawesome/fontawesome-free/js/all'
-// require("../message")
+require('jquery')
+require('../index')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
