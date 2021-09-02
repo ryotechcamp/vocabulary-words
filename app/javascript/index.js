f9515c4fbdd0670d1cb8c7d05cd9dd96607016cc
ryotechcamp <vnvnvnryo0815@gmail.com>
2021-09-03 08:29:22 +0900

    アコーディオン

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -20,4 +20,18 @@ $(function () {
   $('.close-modal').click(function(){
     $('#how-to-modal').fadeOut();
   });
+
+  // アコーディオン
+  $('.faq-list-item').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
+    }
+  });
 });
